63b20ae80c03f9ed99743de2cb3ac0f950142014
alex <lex230323@mail.ru>
2021-05-13 00:25:09 +0400

    add operator_in.py.py

---
+++ operator_in.py
@@ -0,0 +1,11 @@
+hero = 'Superman'
+#hero = 'Super'
+if hero.find('man') != -1:
+    print('Есть')
+
+if 'man' in hero:
+    print('Есть')
+
+goals = 'Научиться питону, накормить кота, здоровье'
+if 'здоровье' in goals:
+    print('Всё хорошо!')
