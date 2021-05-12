b518d592600bc16dc894a9c911a984b1bcc01d9c
alex <lex230323@mail.ru>
2021-05-12 23:38:18 +0400

    add list.py

---
+++ list.py
@@ -0,0 +1,11 @@
+empty_list = []
+friends = ['Max', 'Leo', 'Kate']
+
+print(type(friends))
+
+print('Второй друг: ', friends[1])
+print('Первый друг с конца', friends[-1])
+
+print(friends[1:2])
+print(friends[:2])
+print(friends[1:])
