351f9ca90ee2903a9deb4fb0b7a9cfe7e024fe1f
alex <lex230323@mail.ru>
2021-05-13 16:14:12 +0400

    seq.py

---
+++ seq.py
@@ -0,0 +1,23 @@
+frined_name = 'Max'                     # строка
+friends = ['Max', 'Leo', 'Kate']        # список
+roles = ('admin', 'guest', 'user')      # кортеж tuple
+
+# Типы данных
+print(type(frined_name))
+print(type(friends))
+print(type(roles))
+
+# Индексация
+print(frined_name[1])
+print(friends[1])
+print(roles[1])
+
+# Срезы
+print(frined_name[1:])
+print(friends[1:])
+print(roles[1:])
+
+# Длина
+print(len(frined_name))
+print(len(friends))
+print(len(roles))
