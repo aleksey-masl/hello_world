2ddc1d77c35e9e07de247d614768010254d4c50f
alex <lex230323@mail.ru>
2021-05-15 14:25:41 +0400

    guess_the_number

---
+++ guess_the_number.py
@@ -0,0 +1,17 @@
+# Шаг 1. Загадать случайное число
+import random
+
+number = random.randint(1, 100)
+#print(number)
+
+while True:
+# Шаг 2. Предложить пользователю ввести число
+    user_number = int(input('Введите число: '))
+# Шаг 3. Сравнение чисел. Вывод результата
+    if number == user_number:
+        print("Победа!")
+        break
+    elif number < user_number:
+        print("Ваше число больше загаданного")
+    else:
+        print('Ваше число меньше загаданного')
